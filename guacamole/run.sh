1b64cd3b0923a3ef19204f323921cebf5253a567
Ian Blenke <ian@blenke.com>
2017-11-17 10:11:15 -0500

    Have run.sh script die on errno!=0, make POSTGRES_DATABASE name 12factored

--- guacamole/run.sh
+++ guacamole/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 cat <<EOF > ~/.pgpass
 ${POSTGRES_HOSTNAME:-postgres}:${POSTGRES_PORT:-5432}:${POSTGRES_DATABASE:-postgres}:${POSTGRES_USER:-postgres}:${POSTGRES_PASSWORD:-postgres}
@@ -6,14 +6,13 @@ EOF
 chmod 600 ~/.pgpass
 
 export PGPASSWORD=${POSTGRES_PASSWORD}
+export POSTGRES_DATABASE=${POSTGRS_DATABASE:-guacamole}
 
 if [ ! -f /data/.postgres.initialized ]; then
   createdb -h ${POSTGRES_HOSTNAME:-postgres} -U ${POSTGRES_USER:-postgres} -p ${POSTGRES_PORT:-5432} guacamole
-  /opt/guacamole/bin/initdb.sh --postgres | psql -h ${POSTGRES_HOSTNAME:-postgres} -U ${POSTGRES_USER:-postgres} -p ${POSTGRES_PORT:-5432} guacamole
+  /opt/guacamole/bin/initdb.sh --postgres | psql -h ${POSTGRES_HOSTNAME:-postgres} -U ${POSTGRES_USER:-postgres} -p ${POSTGRES_PORT:-5432} ${POSTGRES_DATABASE}
   touch /data/.postgres.initialized
 fi
-
-export POSTGRES_DATABASE=guacamole
 
 mkdir -p /ssl
 
